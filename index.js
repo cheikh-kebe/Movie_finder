212a390a040157eadf5f799080ff48a0c509e07e
shanks <cheikh.kebe.01@gmail.com>
2021-07-22 21:56:14 +0200

    index

---
+++ index.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("button");
+const wrapper = document.querySelector(".parent");
+
+const getTitle = () =>{
+let title = document.getElementById("title").value
+Request(title)
+}
+async function apikeyToFetch() {
+  var search = document.getElementById("title").value;
+  wrapper.innerHTML= ""
+  await fetch(`http://www.omdbapi.com/?s=${search}&apikey=c4b01846`)
+    .then((response) => response.json())
+    .then((response) => {
+      const movies = response.Search;
+      return movies;
+    })
+    .then((movies) => showMovie(movies))
+    .catch((error) =>{ console.log(error)});
+}
+
+
+const showMovie = (movies) => {
+  movies.map((searchedmovie) => {
+    console.log(searchedmovie);
+    showAllMovies(
+      wrapper,
+      searchedmovie.Title,
+      searchedmovie.Year,
+      searchedmovie.Poster,
+      searchedmovie.Type
+    );
+  });
+};
+
+const showAllMovies = (container, name, year, poster, type) => {
+  
+  container.innerHTML += `
+  <div>
+    <div class="div1"><p>Titre du film : ${name}</p></div>
+    <div class="div2"><p>Année : ${year},type:${type}</p> </div>
+    <div class="div3"><img src="${poster}"></img> </div>
+    <button>read more</button>
+  </div>
+`;
+};
+
+form.addEventListener("click", apikeyToFetch);
